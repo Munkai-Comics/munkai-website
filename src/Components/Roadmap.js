4c4eeff8ac2e9dbf8ac56af1f0fdf6782d9bc4b2
missdobis <missdobis@gmail.com>
2022-03-17 15:45:51 +0100

    roadmap height

--- src/Components/Roadmap.js
+++ src/Components/Roadmap.js
@@ -1,55 +1,35 @@
-
-import Title from "./Title"
-import Panel from "./Panel"
-import {data2022, data2023} from "./Data"
-
+import Title from "./Title";
+import Panel from "./Panel";
+import { data2022, data2023 } from "./Data";
 
 function Roadmap() {
+  return (
+    <div className="body-content roadmap" id="roadmap">
+      <Title text="ROAD MAP" darkBg={true} />
 
-		
-	return(
-		<div className="body-content roadmap" id="roadmap">
+      <div className="roadmap-list d-flex">
+        <div className="roadmap-list-panel">
+          <div className="panel-top-title">2022</div>
 
-				<Title text="ROAD MAP" darkBg={true}/> 
+          <div className="panel-list-box">
+            {data2022.map((data, id) => (
+              <Panel id={id} key={`${id}`} data={data} />
+            ))}
+          </div>
+        </div>
 
-				<div className="roadmap-list d-flex">
-					
-					<div className="roadmap-list-panel">
-						
-						<div className="panel-top-title">2022</div>
-						
-						<div className="panel-list-box">
-								{
-									data2022.map( (data, id) => (
-											<Panel id={id} key={`${id}`} data={data}/>
-									))				
-									
-								}
-						</div>
-					</div>
+        <div className="roadmap-list-panel">
+          <div className="panel-top-title">2023</div>
 
-
-					<div className="roadmap-list-panel">
-						
-						<div className="panel-top-title">2023</div>
-						
-						<div className="panel-list-box">
-								{
-									data2023.map( (data, id) => (
-											<Panel key={`${id}`} id={id} data={data}/>
-									))				
-									
-								}
-						</div>
-					</div>
-
-	
-
-				</div>
-		</div>
-	)
+          <div className="panel-list-box">
+            {data2023.map((data, id) => (
+              <Panel key={`${id}`} id={id} data={data} />
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-
-export default Roadmap
-
+export default Roadmap;
