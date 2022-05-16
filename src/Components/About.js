9bca4b5fc5b2d7471667a4ef488a064f3b22b6a6
missdobis <missdobis@gmail.com>
2022-05-16 20:46:30 +0100

    feat: munkai YouTube video link

--- src/Components/About.js
+++ src/Components/About.js
@@ -9,7 +9,7 @@ function About() {
       <iframe
         className="about-video"
         height={height}
-        src="https://www.youtube-nocookie.com/embed/videoseries?list=PLYgkm0MK8DQGa5_-kK-836SoaOXqjD4rW"
+        src="https://www.youtube.com/embed/55wdWAoQrac"
         title="YouTube video player"
         frameborder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
