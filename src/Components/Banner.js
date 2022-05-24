4e118c21c577520cfaea611b3ccc6a9187fe2cfc
missdobis <missdobis@gmail.com>
2022-05-24 12:30:48 +0100

    feat: nav-link text color, footer text color, hero bg.

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -1,14 +1,15 @@
 function Banner() {
   return (
-    <div className="banner d-flex">
-      <div className="banner-inner-box banner-inner-text">
-        <h1 className="banner-title">COMICS ON THE BLOCKCHAIN</h1>
+    <div className="banner">
+      <div className="banner-hero d-flex">
+        <div className="banner-inner-box banner-inner-text">
+          <h1 className="banner-title">COMICS ON THE BLOCKCHAIN</h1>
 
-        <p className="banner-subtitle">
-          {" "}
-          Creating the first Read to Earn (R2E) comic platform on the blockchain
-          for comic fans, crypto enthusiasts, and creators.
-        </p>
+          <p className="banner-subtitle">
+            Creating the first Read to Earn (R2E) comic platform on the
+            blockchain for comic fans, crypto enthusiasts, and creators.
+          </p>
+        </div>
       </div>
     </div>
   );
